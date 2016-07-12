245b236c926db214a0ad0fa042dea6d0ca3ee1e5
Denise Williams <williamsde@outlook.com>
2016-07-12 15:16:07 -0400

    Working on Select Names

--- basics.sql
+++ basics.sql
@@ -8,4 +8,23 @@ SELECT population FROM world
   SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
   
+  SQL Select Name -- Challenge 1
+  SELECT name FROM world
+  WHERE name LIKE 'Y%'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%y'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%x%'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%land'
+  
+  SELECT name FROM world
+  WHERE name LIKE 'c%ia'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%oo%'
+  
   
