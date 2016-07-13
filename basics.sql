adb69f42355062a35e1ddb2e31f72ccfdd141c1c
Denise Williams <williamsde@outlook.com>
2016-07-13 07:45:23 -0400

    Completed Select Names Challenge

--- basics.sql
+++ basics.sql
@@ -27,4 +27,15 @@ SELECT population FROM world
   SELECT name FROM world
   WHERE name LIKE '%oo%'
   
+  SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
   
+  SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+ 
+ SELECT name FROM world
+ WHERE name LIKE '____'
